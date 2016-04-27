38672794fc9e9e43b45f1e869db4cfb408d7e810
qwemaze <qwelias@gmail.com>
2016-04-27 17:13:03 +0500

    comments for Dima with love

---
+++ many-c3/index.js
@@ -0,0 +1,47 @@
+"use strict";
+
+// documentation: http://c3js.org/reference.html
+
+var chart = c3.generate( {
+	bindto: '#chart',
+	size: {
+		width: 800,
+		height: 900
+	},
+	interaction: {
+		enabled: true // показывать всплывашки по ховеру
+	},
+	data: {
+		json: data,
+		groups: [ // для stacked
+			[ "Здания", "Сооружения", "Машини и оборудование", "Транспортные средства", "Прочие осн. средства" ]
+		],
+		keys: {
+			//x: 'name', // если значения х имеются данных. иначе индекс данных берётся
+			value: [ "Здания", "Сооружения", "Машини и оборудование", "Транспортные средства", "Прочие осн. средства" ],
+		},
+		type: 'area' // 'pie' || 'spline' || 'area' || 'area-spline'
+		// либо для комбинированных объект types, где ключ - имя данных, значение - тип
+	},
+	axis: {
+		x: {
+			// type: 'category', // нужно если по категориям, если нужен таймлайн то 'timeseries'. иначе 'indexed'
+			tick:{
+				// values: new Array(100).fill(0).map(function(v,i){ return (i*0.2).toFixed(1)}) // шаги задаются массивом
+			}
+		},
+        y:{
+			tick:{
+				values: new Array(100).fill(0).map(function(v,i){ return i*10}) // шаги задаются массивом
+			}
+        }
+	},
+    grid:{
+        y:{
+            show: true // показывать грид
+        }
+    },
+	legend:{
+		position: 'bottom' // bottom || right || inset
+	}
+} );
